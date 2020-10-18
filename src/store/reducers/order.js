7bb72ccec393d4f8940b9739f76b138f61ebf31e
musliuarbese <63508538+musliuarbese@users.noreply.github.com>
2020-10-18 14:43:56 +0200

    The Order Reducer

---
+++ src/store/reducers/order.js
@@ -0,0 +1,32 @@
+import * as actionTypes from '../actions/actionTypes';
+
+
+const intialState = {
+   orders: [],
+   loading: false,
+}
+
+const reducer = (state = intialState, actions) =>{
+    switch(action.type){
+        case actionTypes.PURCHSE_BURGER_SUCCESS:
+            const newOrder = {
+                ...action.orderData,
+                id: action.orderId
+            }
+            return{
+                ...state,
+                loading: false,
+                order: state.orders.concat(newOrder)
+                //order osht old orders ku me concat e kthejm nje new array and therefore we add this immutably 
+            };
+        case actionTypes.PURCHASE_BURGER_FAIL:
+            return{
+                ...state,
+                loading: false
+            };
+            default:
+                return state;
+    }
+}
+
+export default reducer;
