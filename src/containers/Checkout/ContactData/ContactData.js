819d30597a92e85fc05e99105b3efe6726a9483a
musliuarbese <63508538+musliuarbese@users.noreply.github.com>
2020-10-20 20:10:50 +0200

    Accesing Protected Resources

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -110,7 +110,7 @@ class ContactData extends Component {
       orderData: formData,
     }
 
-       this.props.onOrderBurger(order);
+       this.props.onOrderBurger(order, this.props.token);
     // axios
     //   .post("/orders.json", order)
     //   .then((response) => {
@@ -211,13 +211,14 @@ class ContactData extends Component {
    return{
      ings: state.burgerBuilder.ingredients,
      price: state.burgerBuilder.totalPrice,
-     loading: state.order.loading
+     loading: state.order.loading,
+     token: state.auth.token
    }
  }
 
  const mapDispatchToProps = dispatch =>{
    return{
-   onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
+   onOrderBurger: (orderData, token) => dispatch(actions.purchaseBurger(orderData, token))
  }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
