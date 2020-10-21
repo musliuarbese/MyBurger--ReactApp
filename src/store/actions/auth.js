92d4031897d3bd196f9cf18256d63763bd8cfb93
musliuarbese <63508538+musliuarbese@users.noreply.github.com>
2020-10-21 15:05:18 +0200

    Persist Auth State with LocalStorage

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -24,6 +24,9 @@ export const authFail = (error) =>{
 }
 
 export const logout = () =>{
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('logout');
     return{
         type: actionTypes.AUTH_LOGOUT
     }
@@ -52,6 +55,10 @@ export const auth = (email, password, isSignup) =>{
         axios.post(url, authData)
          .then(response => {
              console.log(response);
+             const expirationDate = new Date(new Date().getTime + response.data.expirationIn * 1000);
+             localStorage.setItem('token', response.data.idtoken);
+             localStorage.setItem('expirationDate', expirationDate);
+             localStorage.setItem('userId', response.data.localId);
              dispatch(authSuccess(response.data.idtoken, response.data.localId));
              dispatch(checkAuthTimeOut(response.data.expiresIn))
          })
@@ -65,5 +72,23 @@ export const setAuthRedirectPath = (path) =>{
     return{
         type: actionTypes.SET_AUTH_REDIRECT_PATH,
         path: path
+    }
+}
+
+export const authCheckState = () =>{
+    return dispatch =>{
+       const token = localStorage.getItem('token');
+       if(!token){
+         dispatch(logout());
+       }else{
+          const expirationDate = new Date(localStorage.getItem('expirationDate'));
+          if(expirationDate > new Date()){
+              dispatch(logout());
+          }else{
+              const userId = localStorage.getItem('userId');
+              dispatch(authSuccess(token, userId));
+              dispatch(checkAuthTimeOut(expirationDate.getSeconds() - new Date().getSeconds()));
+          }
+       }
     }
 }
